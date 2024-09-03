579cd8c9536a1b0bb0014ff83f41e820bd89334a
James <84340802+zhihaohong52@users.noreply.github.com>
2024-09-03 22:20:26 +0800

    Completed exercises 8 through 12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // Consider only letters and numbers
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    // Convert to lowercase, split to array of individual characters, filter only valid characters, then rejoin as new string
+    const cleanedString = string
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+
+    // Create a new reversed string for comparison
+    const reversedString = cleanedString.split('').reverse().join('');
+
+    // Return the outcome of the comparison which will either be true or false
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    // check argument's type and make sure we use a number throughout rest of function
+    let count;
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg);
+    } else {
+        count = countArg;
+    }
 
+    // check for negative numbers and 0s
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    // initialize first two numbers in sequence
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
 };
 
 // Do not edit below this line
